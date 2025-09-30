cd71bc20b767f88f08b772b6c98c29ce084a44a0
sungjin879 <4sungjin9@gmail.com>
2025-10-01 08:59:17 +0900

    Add files via upload

---
+++ 09-29.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable:4996)
+int main(void)  /*int는 return0 때문*/
+{
+	int nAge = 99;
+	float fHeight = 999.0f;
+	double dWeight = 999.0;
+
+	char chTemp = ' ';
+	char chString[100] = " ";
+
+	printf("Input your alphabet and word:\n");
+	int nResScanf = scanf("%c%s", &chTemp, chString);
+	printf("Output:%d\n", nResScanf);
+	printf("(Alphabet:%c)(Word:%s)\n", chTemp, chString);
+
+	return 0;
+}
+/* 코딩을 고치면 빌드해주기!!!!*/
